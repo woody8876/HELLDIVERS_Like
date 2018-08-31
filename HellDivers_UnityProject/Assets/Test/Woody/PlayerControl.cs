84cf38411dff3a7da298a3ecdc7fb8e012e9dac1
woody8876 <bon8876@gmail.com>
2018-08-31 16:59:56 +0800

    Player Control

---
+++ HellDivers_UnityProject/Assets/Test/Woody/PlayerControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    private float m_Speed=3f;
+    private float m_MoveHorizontal;
+    private float m_MoveVertical;
+    private Vector3 m_CharcterMove;
+    private CharacterController m_CharacterController;
+
+    private float m_MouseX;
+    private float m_MouseY;
+
+    // Use this for initialization
+    void Start () {
+        m_CharacterController = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        m_MoveHorizontal =Input.GetAxis("Horizontal");
+        m_MoveVertical=Input.GetAxis("Vertical");
+
+        m_CharcterMove = (this.transform.forward * m_MoveVertical + this.transform.right * m_MoveHorizontal) * m_Speed * Time.deltaTime;
+        m_CharacterController.Move(m_CharcterMove);
+
+        
+        
+    }
+}
---
+++ HellDivers_UnityProject/Assets/Test/Woody/PlayerForward.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerForward : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 vec;
+
+        Ray r = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit rh;
+
+        Physics.Raycast(r, out rh);
+
+        vec = rh.point - this.transform.position;
+        transform.forward = vec.normalized;
+    }
+}
