26b8dde6ba5b8707afe8268e0a384537cab1d12f
bryan75324 <37688239+bryan75324@users.noreply.github.com>
2018-09-05 14:37:27 +0800

    Update project setting
    + add new input button "Run" - left shift.
    + add new layer "Terrain".

--- HellDivers_UnityProject/Assets/Test/Bryan/Script/PlayerController.cs
+++ HellDivers_UnityProject/Assets/Test/Bryan/Script/PlayerController.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    /// <summary>
+    /// Set character walk speed.
+    /// Runing speed is double the speed.
+    /// </summary>
     public float Speed { get { return m_Speed; } set { m_Speed = value; } }
-    public CharacterController Controller { get; private set; }
 
     [SerializeField] private float m_Speed = 1.1f;
 
     // Use this for initialization
     private void Start()
     {
-        Controller = this.GetComponent<CharacterController>();
+        m_Controller = this.GetComponent<CharacterController>();
 
         if (Camera.main != null)
         {
@@ -30,14 +33,20 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            if (Controller.isGrounded == false)
+            if (m_Controller.isGrounded == false)
             {
-                Controller.Move(Physics.gravity * Time.deltaTime);
+                m_Controller.Move(Physics.gravity * Time.deltaTime);
             }
         }
 
         if (Input.GetMouseButton(0) || Input.GetMouseButton(1)) FaceDirection();
     }
+
+    /*----------------------------------------------------------
+     * Character move with input "Horizontal" & "Vertical".
+     * Direction is reference to camera position.
+     * If doesn't have camera, it's reference to world position.
+     ----------------------------------------------------------*/
 
     private void Move()
     {
@@ -65,39 +74,48 @@ public class PlayerController : MonoBehaviour
             m_Move *= 2.0f;
         }
 
-        if (Controller.isGrounded == false)
+        if (m_Controller.isGrounded == false)
         {
             m_Move += Physics.gravity * Time.deltaTime;
         }
 
         this.transform.forward = m_Direction;
-        Controller.Move(m_Move);
+        m_Controller.Move(m_Move);
     }
+
+    /*---------------------------------
+     * Character face to mouse position
+     ----------------------------------*/
 
     private void FaceDirection()
     {
-        mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(mouseRay, out mouseHit, 1000.0f, 1 << LayerMask.NameToLayer("Terrain")))
+        m_MouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(m_MouseRay, out m_MouseHit, 1000.0f, 1 << LayerMask.NameToLayer("Terrain")))
         {
-            m_Direction = mouseHit.point - this.transform.position;
+            m_Direction = m_MouseHit.point - this.transform.position;
             m_Direction.y = 0.0f;
 
             if (m_Direction.magnitude < 0.1f) return;
 
-            Controller.transform.forward = m_Direction;
+            m_Controller.transform.forward = m_Direction;
         }
     }
 
+    private CharacterController m_Controller;
     private Transform m_Cam;
     private Vector3 m_CamFoward;
     private Vector3 m_Direction;
     private Vector3 m_Move;
-    private Ray mouseRay;
-    private RaycastHit mouseHit;
+    private Ray m_MouseRay;
+    private RaycastHit m_MouseHit;
+
+#if UNITY_EDITOR
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.cyan;
         Gizmos.DrawLine(this.transform.position, this.transform.position + this.transform.forward * 2.0f);
     }
+
+#endif
 }
