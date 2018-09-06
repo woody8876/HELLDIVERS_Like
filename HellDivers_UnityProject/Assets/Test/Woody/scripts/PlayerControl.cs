4a57ff5f44ec087aa1fc78f0bc30b8be3815f0ab
woody8876 <bon8876@gmail.com>
2018-09-06 16:58:46 +0800

    LineRender

--- HellDivers_UnityProject/Assets/Test/Woody/scripts/PlayerControl.cs
+++ HellDivers_UnityProject/Assets/Test/Woody/scripts/PlayerControl.cs
@@ -2,105 +2,117 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerControl : MonoBehaviour
 {
+    public float Speed { get { return m_Speed; } set { m_Speed = value; } }
 
-    private float m_MoveHorizontal;
-    private float m_MoveVertical;
-    private Vector3 m_CharcterMove;
-    private Vector3 m_StandForward;
-    private float m_Speed;
-    private float m_SlowSpeed = 1.5f;
-    private float m_WalkSpeed = 3f;
-    private float m_RunSpeed = 6f;
-    private CharacterController m_CharacterController;
+    [SerializeField] private float m_Speed = 1.1f;
 
-    Vector3 m_MousePos;
-    Ray r;
-    RaycastHit rh;
-    private GameObject m_Body;
-
-    private GameObject LineRenderGameObject;
-    private LineRenderer lineRenderer;
-    private int lineLength = 2;
-
-    void Start()
+    private AimLine m_AimLine;
+    
+    private void Start()
     {
-        m_CharacterController = GetComponent<CharacterController>();
-        LineRenderGameObject = GameObject.Find("Line");
-        lineRenderer = (LineRenderer)LineRenderGameObject.GetComponent("LineRenderer");
-        m_Body = GameObject.FindGameObjectWithTag("Player");
+        m_Controller = this.GetComponent<CharacterController>();
+
+        if (Camera.main != null)
+        {
+            m_Cam = Camera.main.transform;
+        }
+
+        m_AimLine = new AimLine();
     }
 
-
-    void Update()
+    // Update is called once per frame
+    private void Update()
     {
-        m_MoveHorizontal = Input.GetAxis("Horizontal");
-        m_MoveVertical = Input.GetAxis("Vertical");
-        m_Speed = (Input.GetKey(KeyCode.LeftShift)) ? m_RunSpeed : m_WalkSpeed;
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            Move();
+        }
+        else if (m_Controller.isGrounded == false)
+            {
+                m_Controller.Move(Physics.gravity * Time.deltaTime);
+            }
 
-        m_MousePos = Input.mousePosition;
-        r = Camera.main.ScreenPointToRay(m_MousePos);
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1)) FaceDirection();
 
-
-        m_CharcterMove = (this.transform.forward * m_MoveVertical + this.transform.right * m_MoveHorizontal) * m_Speed * Time.deltaTime;
-        m_CharcterMove += Physics.gravity * Time.deltaTime;
-        m_CharacterController.Move(m_CharcterMove);
-
-
-        //紅外線
         if (Input.GetMouseButton(1))
         {
-            m_CharacterController.Move(m_CharcterMove*-0.5f);
-            LineRenderInit();
-            lineRenderer.SetPosition(0,Vector3.forward * 50f);
-            FaceToRay(r,rh,1000);
+            m_AimLine.Init();
+            m_AimLine.SetAimLine();
+        }
+        else if(Input.GetMouseButtonUp(1))
+        {
+            m_AimLine.CloseAimLine();
+        }
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        if (m_Cam != null)
+        {
+            m_CamFoward = Vector3.Scale(m_Cam.forward, Vector3.forward + Vector3.right);
+            m_Direction = m_CamFoward * v + m_Cam.right * h;
         }
         else
         {
-            lineRenderer.enabled = false;
+            m_Direction = Vector3.forward * v + Vector3.right * h;
         }
 
-        //角色面相移動的方向
-        if (Input.GetMouseButton(1) == false)
+        if (m_Direction.magnitude > 1)
         {
-            if (Input.GetKey(KeyCode.W) == false && Input.GetKey(KeyCode.S) == false && Input.GetKey(KeyCode.A) == false && Input.GetKey(KeyCode.D) == false)
-            {
-
-            }
-            else
-            {
-                m_CharcterMove.y = 0.0f;
-                m_Body.transform.forward = m_CharcterMove;
-            }
+            m_Direction.Normalize();
         }
-    }
 
-    private void FaceToRay(Ray r,RaycastHit rh,int Distance)
-    {
-        if (Physics.Raycast(r, out rh, 1000.0f))
+        m_Move = m_Direction * m_Speed * Time.deltaTime;
+
+        if (Input.GetButton("Run"))
         {
-            Vector3 vTarget = rh.point;
-            Vector3 vForward = vTarget - m_Body.transform.position;
-            vForward.y = 0.0f;
+            m_Move *= 2.0f;
+        }
 
-            vForward.Normalize();
-            m_Body.transform.forward = vForward;
+        if (m_Controller.isGrounded == false)
+        {
+            m_Move += Physics.gravity * Time.deltaTime;
+        }
+
+        this.transform.forward = m_Direction;
+        m_Controller.Move(m_Move);
+    }
+    
+
+    private void FaceDirection()
+    {
+        m_MouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(m_MouseRay, out m_MouseHit, 1000.0f, 1 << LayerMask.NameToLayer("Terrain")))
+        {
+            m_Direction = m_MouseHit.point - this.transform.position;
+            m_Direction.y = 0.0f;
+
+            if (m_Direction.magnitude < 0.1f) return;
+
+            m_Controller.transform.forward = m_Direction;
         }
     }
-    private void LineRenderInit()
-    {
-        lineRenderer.enabled = true;
-        lineRenderer.useWorldSpace = false;
-        lineRenderer.startWidth = 0.1f;
-        lineRenderer.endWidth = 0.1f;
-        lineRenderer.positionCount = lineLength;
-    }
+
+    private CharacterController m_Controller;
+    private Transform m_Cam;
+    private Vector3 m_CamFoward;
+    private Vector3 m_Direction;
+    private Vector3 m_Move;
+    private Ray m_MouseRay;
+    private RaycastHit m_MouseHit;
+
+#if UNITY_EDITOR
+
     private void OnDrawGizmos()
     {
-        Vector3 target1;
-        target1 = m_Body.transform.forward * 10 + this.transform.position;
-        Gizmos.DrawLine(this.transform.position, target1);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(this.transform.position, this.transform.position + this.transform.forward * 2.0f);
     }
-}
 
+#endif
+}
