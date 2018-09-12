2dc995d34554cfc5748e031ea8151ff922e45e57
woody8876 <bon8876@gmail.com>
2018-09-12 17:08:07 +0800

    LinederGuyLiang

--- HellDivers_UnityProject/Assets/Test/Woody/scripts/AimLine.cs
+++ HellDivers_UnityProject/Assets/Test/Woody/scripts/AimLine.cs
@@ -7,20 +7,16 @@ public class AimLine : MonoBehaviour
     private Transform m_LineRenderTransform;
     private Transform m_Enitter;
     private LineRenderer m_LineRenderer;
+    private GameObject m_LineRender;
     private int straightPosCount = 2;
     private int spinePosCount = 50;
 
     private void Start()
     {
-        m_LineRenderTransform = transform.Find("LineRender");
-        if (m_LineRenderTransform == null)
-        {
-            Debug.Log("Can't find the LineRender");
-        }
-        else
-        {
-            m_LineRenderer = (LineRenderer)m_LineRenderTransform.GetComponent("LineRenderer");
-        }
+        m_LineRender =  Resources.Load("LineRender") as GameObject;
+        m_LineRender = Instantiate(m_LineRender, this.transform);
+        m_LineRenderer = m_LineRender.GetComponent<LineRenderer>();
+        
         SetAimLineInfo(true);
     }
     private void Update()
@@ -37,7 +33,7 @@ public class AimLine : MonoBehaviour
         {
             SetAimLineInfo(true);
         }
-        else if(Input.GetKeyDown(KeyCode.X))
+        else if (Input.GetKeyDown(KeyCode.X))
         {
             SetAimLineInfo(false);
         }
@@ -54,30 +50,33 @@ public class AimLine : MonoBehaviour
 
     private void OpenAimLine()
     {
+        
         m_LineRenderer.enabled = true;
+        
         m_Enitter = transform.Find("Emitter");
 
         if (m_Enitter == null)
         {
             Debug.Log("Can't find the Emitter");
-            m_LineRenderer.SetPosition(0, this.transform.position);
+            m_LineRenderer.SetPosition(0, new Vector3(0,0,0));
             m_LineRenderer.SetPosition(1, (GetLastPosition()));
         }
-        else if(m_LineRenderer.positionCount == straightPosCount)
+        if (m_LineRenderer.positionCount == straightPosCount)
         {
             m_LineRenderer.SetPosition(0, m_Enitter.transform.localPosition);
-            m_LineRenderer.SetPosition(1, (GetLastPosition()));
+            m_LineRenderer.SetPosition(1, GetLastPosition());
+            Debug.Log(m_LineRenderer.enabled);
+
         }
         else if (m_LineRenderer.positionCount == spinePosCount)
         {
-            Vector3 vPosition0 = m_Enitter.transform.localPosition ;
-            Vector3 vPosition1 = m_Enitter.transform.localPosition ;
+            Vector3 vPosition0 = m_Enitter.transform.localPosition;
+            Vector3 vPosition1 = m_Enitter.transform.localPosition;
             Vector3 vPosition2 = (GetLastPosition() + m_Enitter.transform.localPosition) / 3;
             vPosition2.y = 10f;
             Vector3 vPosition3 = (GetLastPosition() + m_Enitter.transform.localPosition) / 3 * 2;
             vPosition3.y = 10f;
             Vector3 vPosition4 = GetLastPosition();
-            
             float test = 50;
 
             for (int i = 0; i < spinePosCount; i++)
@@ -95,7 +94,7 @@ public class AimLine : MonoBehaviour
                 Vector3 pos2_1 = Vector3.Lerp(pos1_1, pos1_2, i / test);
 
                 Vector3 find = Vector3.Lerp(pos2_0, pos2_1, i / test);
-                
+
                 m_LineRenderer.SetPosition(i, find);
             }
         }
@@ -109,24 +108,12 @@ public class AimLine : MonoBehaviour
     {
         if (m_LineRenderer.positionCount == straightPosCount)
         {
-            return new Vector3(0, 0, 50f);
-
+            Debug.Log("straight");
+            return new Vector3(0, 0, 50);
         }
         else if (m_LineRenderer.positionCount == spinePosCount)
         {
-            Ray MouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit MouseHit;
-
-            if (Physics.Raycast(MouseRay, out MouseHit, 1000.0f, 1 << LayerMask.NameToLayer("Terrain")))
-            {
-                Vector3 mPos = MouseHit.point;
-                mPos.y = 0;
-                Vector3 ePos = this.transform.position;
-                ePos.y = 0;
-                Vector3 Distance = mPos - ePos;
-
-                return new Vector3(0, 0, Distance.magnitude);
-            }
+            return new Vector3(0, 0, 10);
         }
         return new Vector3(0, 0, 0);
     }
