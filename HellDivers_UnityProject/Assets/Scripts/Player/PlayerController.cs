9e745d02307bfb1bca1aa6b636cc19263f80a542
woody8876 <bon8876@gmail.com>
2018-10-05 12:16:18 +0800

    Revert "Update ch00"
    This reverts commit ae10dcecb17056242ee2fbbc8c013a2105e50e47.

--- HellDivers_UnityProject/Assets/Scripts/Player/PlayerController.cs
+++ HellDivers_UnityProject/Assets/Scripts/Player/PlayerController.cs
@@ -4,35 +4,83 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float Speed { get { return m_Speed; } set { m_Speed = value; } }
+
+    [SerializeField] private float m_Speed = 5f;
 
     #region Private Variable
+
     private CharacterController m_Controller;
     private Transform m_Cam;
-    private Vector3 m_CamForward;
-    private Vector3 m_Move;
+    private Vector3 m_CamFoward;
     private Vector3 m_Direction;
+    private Vector3 m_Move;
     private Ray m_MouseRay;
     private RaycastHit m_MouseHit;
-    private PlayerAnimationsContorller PAC;
+    private bool bChange;
     #endregion Private Variable
-
-    private bool bRun = false;
-    private bool bInBattle = false;
-    private bool bAttack = false;
 
     private void Start()
     {
-        PAC = this.GetComponent<PlayerAnimationsContorller>();
         m_Controller = this.GetComponent<CharacterController>();
 
         if (Camera.main != null)
         {
             m_Cam = Camera.main.transform;
         }
+        bChange = true;
     }
-    private void FixedUpdate()
+
+    private void Update()
     {
-        Move();
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            Move();
+        }
+        else
+        {
+            PlayerAnimationsContorller.m_MoveState = ePlayerAnimationState.ANI_IDLE;
+        }
+        if (m_Controller.isGrounded == false)
+        {
+            m_Controller.Move(Physics.gravity * Time.deltaTime);
+        }
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
+        {
+            FaceDirection();
+            Attack();
+        }
+        #region DisplayAnimation
+        if (PlayerAnimationsContorller.m_AttackState != ePlayerAttack.ANI_THROWSTANDBY)
+        {
+            if ((Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Alpha2)))
+            {
+                PlayerAnimationsContorller.m_AttackState = ePlayerAttack.ANI_SWITCHWEAPON;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                PlayerAnimationsContorller.m_AttackState = ePlayerAttack.ANI_THROW;
+                Debug.Log("Press");
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                PlayerAnimationsContorller.m_AttackState = ePlayerAttack.ANI_RELOAD;
+            }
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+                PlayerAnimationsContorller.m_MoveState = ePlayerAnimationState.ANI_DEATH;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerAnimationsContorller.m_AnyState = ePlayerAnyState.ANI_ROLL;
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            PlayerAnimationsContorller.m_AnyState = ePlayerAnyState.ANI_GETHURT;
+        }
+        #endregion DisplayAnimation
     }
 
     private void Move()
@@ -40,55 +88,107 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-
         if (m_Cam != null)
         {
-            m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-            m_Move = v * m_CamForward + h * m_Cam.right;
+            m_CamFoward = Vector3.Scale(m_Cam.forward, Vector3.forward + Vector3.right);
+            m_Direction = m_CamFoward.normalized * v + m_Cam.right * h;
         }
         else
         {
-            m_Move = v * Vector3.forward + h * Vector3.right;
+            m_Direction = Vector3.forward * v + Vector3.right * h;
         }
+
+        if (m_Direction.magnitude > 1)
+        {
+            m_Direction.Normalize();
+        }
+
+        m_Move = m_Direction * m_Speed * Time.deltaTime;
+
+        float fAngle = Vector3.Angle(this.transform.forward, m_Direction.normalized);
+        float dotTurnRight = Vector3.Dot(this.transform.right, m_Direction.normalized);
+
+        if (fAngle > 80)
+        {
+            if (dotTurnRight >= 0)
+            {
+                //PlayerAnimationsContorller.m_MoveState = ePlayerAnimationState.ANI_TURNRIGHT90;
+                Debug.Log("Turn Right");
+            }
+            else if (dotTurnRight < 0)
+            {
+                //PlayerAnimationsContorller.m_MoveState = ePlayerAnimationState.ANI_TURNLEFT90;
+                Debug.Log("Turn Left");
+            }
+
+        }
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
+        {
+            PlayerAnimationsContorller.m_MoveState = ePlayerAnimationState.ANI_WALKSHOOT;
+        }
+        else
+        {
+            PlayerAnimationsContorller.m_MoveState = ePlayerAnimationState.ANI_WALK;
+            if (Input.GetButton("Run"))
+            {
+                m_Move *= 3.0f;
+                PlayerAnimationsContorller.m_MoveState = ePlayerAnimationState.ANI_RUN;
+            }
+        }
+
+
+
         if (m_Controller.isGrounded == false)
         {
             m_Move += Physics.gravity * Time.deltaTime;
         }
-        bRun = (Input.GetButton("Run")) ? true : false;
 
-        if (m_Move.magnitude > 1) m_Move.Normalize();
 
-        if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
+        if (fAngle < 80)
         {
-            FaceDirection();
-            bInBattle = true;
-            if (Input.GetMouseButtonDown(0))
-            {
-                bAttack = true;
-            }
         }
-        else bInBattle = false;
+            this.transform.forward = m_Direction;
+            m_Controller.Move(m_Move);
 
-        PAC.Move(m_Move, m_Direction, bRun, bInBattle, bAttack);
+       
     }
 
     private void FaceDirection()
     {
         float vHigh = Camera.main.transform.position.y - this.transform.position.y;
+
         Ray MouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         Vector3 vMouseRay = MouseRay.direction;
-        vMouseRay.Normalize();
 
+        vMouseRay.Normalize();
         float angle = Vector3.Dot(new Vector3(0, -1, 0), vMouseRay);
+
         float distance = vHigh / angle;
         Vector3 endPoint = MouseRay.GetPoint(distance);
-
         m_Direction = endPoint - this.transform.position;
         m_Direction.y = 0.0f;
+
         if (m_Direction.magnitude < 0.1f) return;
-        if (m_Direction.magnitude > 1) m_Direction.Normalize();
+        m_Controller.transform.forward = m_Direction;
     }
+
+    private void Attack()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (PlayerAnimationsContorller.m_AttackState == ePlayerAttack.ANI_GUNPLAY)
+            {
+                PlayerAnimationsContorller.m_AttackState = ePlayerAttack.ANI_SHOOT;
+            }
+            if (PlayerAnimationsContorller.m_AttackState == ePlayerAttack.ANI_THROWSTANDBY)
+            {
+                PlayerAnimationsContorller.m_AttackState = ePlayerAttack.ANI_THROWOUT;
+            }
+        }
+    }
+
 #if UNITY_EDITOR
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.cyan;
