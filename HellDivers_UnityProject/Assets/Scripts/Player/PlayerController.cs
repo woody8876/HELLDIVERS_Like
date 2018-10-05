ccd88ac158237daa8e23d6523df4822fc420222d
woody8876 <bon8876@gmail.com>
2018-10-05 12:15:30 +0800

    Revert "ReSend ch00"
    This reverts commit 27201228acfb84df14e355218c69976d902a94fb.

--- HellDivers_UnityProject/Assets/Scripts/Player/PlayerController.cs
+++ HellDivers_UnityProject/Assets/Scripts/Player/PlayerController.cs
@@ -68,7 +68,11 @@ public class PlayerController : MonoBehaviour
             }
         }
         else bInBattle = false;
-        
+
+        if (true)
+        {
+
+        }
         PAC.Move(m_Move, m_Direction, bRun, bInBattle, bAttack);
     }
 
