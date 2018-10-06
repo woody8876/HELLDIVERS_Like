0a398fcbd3c3dacde75bb3bbf3bb4ae6a543b247
IvanCKP <42830573+IvanCKP@users.noreply.github.com>
2018-10-07 06:43:01 +0800

    camera

--- HellDivers_UnityProject/Assets/Scripts/Player/CameraController.cs
+++ HellDivers_UnityProject/Assets/Scripts/Player/CameraController.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float m_fPosX = 0.0f;
     public float m_fPosY = 10.0f;
-    public float m_fPosZ = -7.5f;
     public float m_fRotX = 60.0f;
-    public float m_fRotY = 0.0f;
-    public float m_fRotZ = 0.0f;
+    private float fRotateX;
 
     public float K;
     public float L;
 
     public Transform m_Player;
     private Camera m_Cam;
-    private Vector3 m_vPrePos;
+    private Vector3 m_vVec;
     private Vector3 m_vCurPos;
 
     // Use this for initialization
     private void Start()
     {
         m_Cam = Camera.main.GetComponent<Camera>();
-        m_Cam.transform.position = new Vector3(m_fPosX, m_fPosY, m_fPosZ);
-        m_Cam.transform.rotation = Quaternion.Euler(m_fRotX, m_fRotY, m_fRotZ);
+        m_Cam.transform.rotation = Quaternion.Euler(m_fRotX, 0, 0);
+        fRotateX = Mathf.Tan((90 - m_fRotX) * Mathf.Deg2Rad) ;
+        Debug.Log(fRotateX * m_fPosY);
+
         m_Cam.fieldOfView = 60f;
         if (m_Player == null) m_Player = this.transform;
     }
@@ -33,15 +32,12 @@ public class CameraController : MonoBehaviour
     private void Update()
     {
         if (m_Player == this.transform) { return; }
-        m_vPrePos = m_Player.position;
+        m_vCurPos = m_Player.position;
     }
     private void LateUpdate()
     {
         if (m_Player == this.transform) { return; }
-        m_vCurPos = m_Player.position;
-        Debug.Log(CameraMove());
-        Debug.LogFormat("PrePos: {0} || CurPos: {1}", m_vPrePos, m_vCurPos);
-        if (!CameraMove()) { FollowTarget(m_vCurPos); }
+        FollowTarget(m_vCurPos); 
     }
     
 
@@ -50,27 +46,21 @@ public class CameraController : MonoBehaviour
         return m_player.position;
     }
 
-    private bool CameraMove()
+    private Vector3 CameraMove()
     {
-        if (m_vCurPos != m_vPrePos)
-        {
-            Vector3 vCurCamPos = m_Cam.transform.position;
-            Vector3 Direc = m_vCurPos - m_vPrePos;
-            Direc.Normalize();
-            vCurCamPos.x = Mathf.Lerp(vCurCamPos.x, m_vCurPos.x + Direc.x * K + m_fPosX, L);
-            vCurCamPos.z = Mathf.Lerp(vCurCamPos.z, m_vCurPos.z + Direc.z * K * 0.75f + m_fPosZ, L * 0.75f);
-            m_Cam.transform.position = vCurCamPos;
-            return true;
-        }
-        return false;
+        m_vVec.x = Input.GetAxis("Horizontal") * K;
+        float z = Input.GetAxis("Vertical") * K * 0.75f;
+        m_vVec.z = (z > 0) ? z : z * 1.25f;
+        return m_vVec;
     }
 
     private void FollowTarget(Vector3 Target)
     {
         Vector3 vCurCamPos = m_Cam.transform.position;
-        vCurCamPos.x = Mathf.Lerp(vCurCamPos.x, Target.x + m_fPosX, 2f * Time.deltaTime);
-        vCurCamPos.y = Mathf.Lerp(vCurCamPos.y, Target.y + m_fPosY, 2f * Time.deltaTime);
-        vCurCamPos.z = Mathf.Lerp(vCurCamPos.z, Target.z + m_fPosZ, 2f * Time.deltaTime);
+        float fzPos = -fRotateX * (m_fPosY-1f);
+        vCurCamPos.x = Mathf.Lerp(vCurCamPos.x, Target.x + CameraMove().x, 2f * Time.deltaTime);
+        vCurCamPos.y = Mathf.Lerp(vCurCamPos.y, Target.y + CameraMove().y + m_fPosY, 2f * Time.deltaTime);
+        vCurCamPos.z = Mathf.Lerp(vCurCamPos.z, Target.z + CameraMove().z + fzPos, 2f * Time.deltaTime);
         m_Cam.transform.position = vCurCamPos;
     }
 
