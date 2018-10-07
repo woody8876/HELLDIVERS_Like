e9cf6cefc19abf57b705b570dfe595542e84e44a
BryanHsiao <37688239+bryan75324@users.noreply.github.com>
2018-10-07 16:30:25 +0800

    player data

---
+++ HellDivers_UnityProject/Assets/Test/Bryan/Script/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerData
+{
+    private string username;
+    private string nickname;
+    private int rank;
+    private int totalMissionFought;
+    private int totalMissionWon;
+    private int totalDeaths;
+    private int totalShotsFired;
+    private int totalEnemiesKilled;
+    private string[] weapons;
+    private string[] stratagems;
+}
