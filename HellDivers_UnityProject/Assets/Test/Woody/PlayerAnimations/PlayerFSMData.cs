c30cac15aefece9eaafff622c033ddc5c9599b05
woody8876 <bon8876@gmail.com>
2018-10-24 19:14:51 +0800

    fix PerformPlayer

--- HellDivers_UnityProject/Assets/Test/Woody/PlayerAnimations/PlayerFSMData.cs
+++ HellDivers_UnityProject/Assets/Test/Woody/PlayerAnimations/PlayerFSMData.cs
@@ -7,6 +7,7 @@ public class PlayerFSMData
     public PlayerFSMSystem m_PlayerFSMSystem;
     public bool m_FinishAni = false;
     public string m_NowAnimation = "Origin";
+    public PlayerController m_PlayerController;
     public CharacterController m_CharacterController;
     public PlayerAnimationsContorller m_AnimationController;
     public WeaponController m_WeaponController;
