367167d61f77f0f044ada91533428d9661f4d998
BryanHsiao <37688239+bryan75324@users.noreply.github.com>
2018-10-30 20:27:45 +0800

    UPDATE In-game UI : player info

---
+++ HellDivers_UnityProject/Assets/Test/Bryan/Script/UIPlayerInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPlayerInfo : MonoBehaviour
+{
+    public Player CurrentPlayer { get { return m_Player; } }
+    private Player m_Player;
+    private Text m_PlayerName;
+
+    public void Initialize(Player player)
+    {
+        m_Player = player;
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
