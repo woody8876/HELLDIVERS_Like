10a918a901d0f745d6092fa61ada1368961f3d79
BryanHsiao <37688239+bryan75324@users.noreply.github.com>
2018-10-31 14:19:13 +0800

    UPDATE In-game UI player info (WIP)

--- HellDivers_UnityProject/Assets/Test/Bryan/Script/UIPlayerInfo.cs
+++ HellDivers_UnityProject/Assets/Test/Bryan/Script/UIPlayerInfo.cs
@@ -8,6 +8,8 @@ public class UIPlayerInfo : MonoBehaviour
     public Player CurrentPlayer { get { return m_Player; } }
     private Player m_Player;
     [SerializeField] private GameObject m_Title;
+    private Text m_PlayerName;
+    private Text m_PlayerRank;
 
     public void Initialize(Player player)
     {
@@ -17,10 +19,19 @@ public class UIPlayerInfo : MonoBehaviour
     // Use this for initialization
     private void Start()
     {
+        if (m_Title != null)
+        {
+            m_PlayerName = m_Title.transform.Find("Text_Name").GetComponent<Text>();
+            m_PlayerRank = m_Title.transform.Find("Text_Rank").GetComponent<Text>();
+        }
     }
 
     // Update is called once per frame
     private void Update()
+    {
+    }
+
+    private void UpdateTitleDisplay(Player player)
     {
     }
 }
