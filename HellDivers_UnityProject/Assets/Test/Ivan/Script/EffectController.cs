8fe7dfd97c4fbf9f29768fd87ee91806736bcdc8
IvanCKP <42830573+IvanCKP@users.noreply.github.com>
2018-11-06 02:15:27 +0800

    Fiexd Grenades

---
+++ HellDivers_UnityProject/Assets/Test/Ivan/Script/EffectController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectController : MonoBehaviour {
+
+    [SerializeField] int m_ID;
+
+    Animator m_Animator;
+    float m_fTime;
+    bool m_bFinish;
+
+
+    private void UnLoadObject()
+    {
+        ObjectPool.m_Instance.UnLoadObjectToPool(m_ID, this.gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+        m_Animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (m_bFinish)
+        {
+            m_bFinish = false;
+            UnLoadObject();
+        }
+	}
+}
