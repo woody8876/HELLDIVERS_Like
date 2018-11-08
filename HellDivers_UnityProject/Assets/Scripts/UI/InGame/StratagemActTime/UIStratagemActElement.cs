17c0daeacce85e6d4d5f87895ab31d13f3b22c50
BryanHsiao <37688239+bryan75324@users.noreply.github.com>
2018-11-08 19:09:47 +0800

    Update UI

--- HellDivers_UnityProject/Assets/Scripts/UI/InGame/StratagemActTime/UIStratagemActElement.cs
+++ HellDivers_UnityProject/Assets/Scripts/UI/InGame/StratagemActTime/UIStratagemActElement.cs
@@ -19,10 +19,35 @@ namespace HELLDIVERS.UI.InGame
 
             string fileName = string.Format("icon_{0}", stratagem.Info.ID);
             m_Icon.sprite = UIHelper.LoadSprite(UIHelper.StratagemIconFolder, fileName);
+
+            SubscribePlayerEvent();
+        }
+
+        private void OnDestroy()
+        {
+            UnsubscribePlayerEvent();
+        }
+
+        private void SubscribePlayerEvent()
+        {
+            CurrentStratagem.OnStartActivation += RefreshUI;
+            CurrentStratagem.OnActivation += RefreshUI;
+            CurrentStratagem.OnEndActivation += RefreshUI;
+        }
+
+        private void UnsubscribePlayerEvent()
+        {
+            CurrentStratagem.OnStartActivation -= RefreshUI;
+            CurrentStratagem.OnActivation -= RefreshUI;
+            CurrentStratagem.OnEndActivation -= RefreshUI;
         }
 
         private void RefreshUI()
         {
+            string minutes = Mathf.Floor(CurrentStratagem.ActTimeCountDown / 60).ToString("00");
+            string seconds = (CurrentStratagem.ActTimeCountDown % 60).ToString("00");
+            string minisec = Mathf.Floor((CurrentStratagem.ActTimeCountDown * 100) % 100).ToString("00");
+            m_Time.text = string.Format("{0}:{1}:{2}", minutes, seconds, minisec);
         }
 
         // Use this for initialization
