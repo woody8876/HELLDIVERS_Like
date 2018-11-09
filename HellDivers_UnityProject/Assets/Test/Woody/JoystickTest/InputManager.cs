5e44e5043ede3c7a0e6e1e47b768269247fd1da5
woody8876 <bon8876@gmail.com>
2018-11-09 15:30:39 +0800

    InputTable

---
+++ HellDivers_UnityProject/Assets/Test/Woody/JoystickTest/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+    
+
+    public Dictionary<int, InputInfo> PlayerInputInfoTable;
+    // Use this for initialization
+    private void Awake()
+    {
+        PlayerInputInfoTable = new Dictionary<int, InputInfo>();
+    }
+    void Start () {
+        PlayerInputInfoTable = PlayerInputLoader.LoadData("PlayerInputTable");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+        }
+	}
+}
---
+++ HellDivers_UnityProject/Assets/Test/Woody/JoystickTest/PlayerInputLoader.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputLoader {
+
+    public static Dictionary<int, InputInfo> LoadData(string filePath)
+    {
+        Dictionary<int, InputInfo> playerInputInfo = new Dictionary<int, InputInfo>();
+        if (_LoadDataBase(filePath, ref playerInputInfo) == true)
+        {
+            return playerInputInfo;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    private static bool _LoadDataBase(string tablePath, ref Dictionary<int, InputInfo> Info)
+    {
+        Info.Clear();
+        TextAsset datas = Resources.Load<TextAsset>(tablePath);
+        if (datas != null)
+        {
+            string[] lines = datas.text.Split('\n');
+            for (int i = 1; i < lines.Length - 1; i++)
+            {
+                string[] playerInputInfo = lines[i].Split(',');
+                InputInfo data = new InputInfo();
+                data.SetID(int.Parse(playerInputInfo[0]));
+                data.SetHorizontal(playerInputInfo[1]);
+                data.SetVertical(playerInputInfo[2]);
+                data.SetDirectionHorizontal(playerInputInfo[3]);
+                data.SetDirectionVertical(playerInputInfo[4]);
+                data.SetStratagemHorizontal(playerInputInfo[5]);
+                data.SetStratagemVertical(playerInputInfo[6]);
+                data.SetFire(playerInputInfo[7]);
+                data.SetStratagem(playerInputInfo[8]);
+                data.SetRun(playerInputInfo[9]);
+                data.SetWeaponSwitch(playerInputInfo[10]);
+                data.SetReload(playerInputInfo[11]);
+                data.SetMeleeAttack(playerInputInfo[12]);
+                data.SetInteractive(playerInputInfo[13]);
+                data.SetRoll(playerInputInfo[14]);
+                Info.Add(data.ID, data);
+            }
+            return true;
+        }
+        return false;
+    }
+}
