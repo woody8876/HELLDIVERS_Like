03bc0a15ec7afe838eb5f28859e92351d9b7e06d
BryanHsiao <37688239+bryan75324@users.noreply.github.com>
2018-11-14 21:33:32 +0800

    Fix weapon info data reference
    + Fix weapon info data reference
    + Fix bullet raycast hit.

--- HellDivers_UnityProject/Assets/Scripts/Item/InteractiveItems/AmmoPackage.cs
+++ HellDivers_UnityProject/Assets/Scripts/Item/InteractiveItems/AmmoPackage.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class AmmoPackage : InteractiveItem
 {
-    public int WeaponID { get { return m_WeaponID; } }
-    [SerializeField] private int m_WeaponID;
-
-    public void Initialize(int weaponId)
-    {
-        m_WeaponID = weaponId;
-    }
-
     public override void OnInteract(Player player)
     {
-        m_WeaponID = player.WeaponController._CurrentWeapon;
         int[] ids = player.WeaponController.ActivedWeaponID;
         bool bFillUp = false;
         for (int i = 0; i < ids.Length; i++)
         {
             int currentWeaponId = player.WeaponController.ActivedWeaponID[i];
-            int num = Mathf.RoundToInt(player.WeaponController.ActiveWeapon[currentWeaponId].weaponInfo.Start_Mags);
-            bFillUp = player.WeaponController.AddMags(currentWeaponId, num);
+            int num = player.WeaponController.ActiveWeapon[currentWeaponId].weaponInfo.Start_Mags;
+            bool bCurrentFillUp = player.WeaponController.AddMags(currentWeaponId, num);
+            if (bCurrentFillUp) bFillUp = true;
         }
         if (bFillUp == false) return;
 
