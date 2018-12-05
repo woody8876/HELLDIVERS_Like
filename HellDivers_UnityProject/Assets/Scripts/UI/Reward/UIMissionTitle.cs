e777f9c180b80fabc7be466bcdd2ec38338ab580
BryanHsiao <37688239+bryan75324@users.noreply.github.com>
2018-12-05 14:03:09 +0800

    UPDTAE Reward scene.
    + Add mission failed.

---
+++ HellDivers_UnityProject/Assets/Scripts/UI/Reward/UIMissionTitle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HELLDIVERS.UI
+{
+    [RequireComponent(typeof(UITweenCanvasAlpha))]
+    public class UIMissionTitle : MonoBehaviour
+    {
+        public UITweenCanvasAlpha CanavasTween { get { return m_CanvasTween; } }
+
+        [SerializeField] private Text m_Title;
+        private UITweenCanvasAlpha m_CanvasTween;
+
+        private void Awake()
+        {
+            m_CanvasTween = this.GetComponent<UITweenCanvasAlpha>();
+        }
+
+        public void Initialize(bool success)
+        {
+            if (success)
+            {
+                m_Title.text = "MISSION COMPLETE";
+            }
+            else
+            {
+                m_Title.text = "MISSION FAILED";
+            }
+        }
+    }
+}
